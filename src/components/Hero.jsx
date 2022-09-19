b1174a3e0490d71a6c796caa3a316a1b649ffa98
Daniel-Magar <daniel.t@ekoinfomatics.com>
2022-09-19 18:24:37 +0530

    links

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import heroblur from "../img/herroblur.png";
 import eko1 from "../img/eko1.gif";
 import h1 from "../img/h1.png";
+import rightUpArrow from "../img/rightUpArrow.png";
+import linkLine from "../img/linkLine.png";
+import whoArrow1 from "../img/whoArrow1.png";
+import { Button1, Button2, Button3 } from "./common/HeroHlink";
 
 const Hero = () => {
   const [linkText, setLinkText] = useState([
@@ -30,6 +34,26 @@ const Hero = () => {
     ["Our Capabilites ", "in AI ML ", "and", "Dashboard", "Analytics."],
   ]);
 
+  const [btn1, setBtn1] = useState(true);
+  const [btn2, setBtn2] = useState(false);
+  const [btn3, setBtn3] = useState(false);
+  const changeText = (id) => {
+    if (id == 1) {
+      setBtn1(true);
+      setBtn2(false);
+      setBtn3(false);
+    } else if (id == 2) {
+      setBtn2(true);
+      setBtn1(false);
+      setBtn3(false);
+    } else if (id == 3) {
+      setBtn3(true);
+      setBtn2(false);
+      setBtn1(false);
+    } else {
+      setBtn1(true);
+    }
+  };
   // useEffect(() => {
   //   setLinkText(arr);
   // }, [arr]);
@@ -60,35 +84,144 @@ const Hero = () => {
             </div>
           </div>
           <div>
-            <div className="mt-[4rem]">
-              <div className="  lg:w-[53rem] h-[5.5rem] border-bright-blue  border-l-[1px] border-b-[1px] relative">
-                <div className="bg-main-black h-[0.5rem] w-[2.2rem] absolute top-[5.2rem] left-[3rem] flex justify-center items-center"></div>
-                <p className="text-fade-gray text-[1rem] lg:text-[1.5rem] xl:text-[2rem] p-3 ">
-                  {linkText[0]}
-                  <span className="text-white font-bold">{linkText[1]}</span>
-                  &nbsp; {linkText[2]} &nbsp;
-                  <span className="text-white font-bold">{linkText[3]}</span>
-                  &nbsp; {linkText[4]}
-                </p>
+            <div className=" mt-[4rem] mx-1rem  min-h-[7rem] max-h-full border border-l-bright-blue border-b-bright-blue border-t-0 border-r-0 relative">
+              <div className="  lg:w-[53rem] min-h-[7rem]">
+                <div>
+                  {btn1 == true ? <Button1 /> : ""}
+                  {btn2 == true ? <Button2 /> : ""}
+                  {btn3 == true ? <Button3 /> : ""}
+
+                  {/* <p className="text-fade-gray text-[1rem] lg:text-[1.5rem] xl:text-[2rem] p-3 ">
+                    {linkText[0]}
+                    <span className="text-white font-bold">{linkText[1]}</span>
+                    &nbsp; {linkText[2]} &nbsp;
+                    <span className="text-white font-bold">{linkText[3]}</span>
+                    &nbsp; {linkText[4]}
+                  </p> */}
+                  <div className="flex  min-h-[1.5rem] max-h-[3rem] text-white absolute left-0 bottom-0 right-0 top-[6.5rem]">
+                    <div className="  w-[4rem] max-w-[12rem]"></div>
+                    <div className="  w-[5rem] bg-main-black"></div>
+                    <div className="  w-full"></div>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className=" mt-[4rem] text-white ">
-              <div className="list-none mb-3  font-semibold">
-                <button onClick={() => setLinkText(arr[0])}>
-                  Who are we ?
-                </button>
-              </div>
-              <div className="list-none mb-3">
-                <button onClick={() => setLinkText(arr[1])}>
-                  What we do ?
-                </button>
-              </div>
-              <div className="list-none ">
-                <button onClick={() => setLinkText(arr[2])}>
-                  We have delivered
-                </button>
+            {/* New Menu */}
+            <div className=" text-white text-[1rem] lg:text-[1.5rem] xl:text-[2rem]">
+              <div className="flex justify-start items-start">
+                <div className="flex flex-col gap-4">
+                  <div>
+                    <div className=" mt-12">
+                      <div className="list-none font-semibold text-left">
+                        <button
+                          onClick={() => changeText(1)}
+                          className="text-left"
+                        >
+                          Who are we ?
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="list-none ">
+                      <button
+                        onClick={() => changeText(2)}
+                        className="text-left"
+                      >
+                        What we do ?
+                      </button>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="list-none text-left ">
+                      <button
+                        onClick={() => changeText(3)}
+                        className="text-left"
+                      >
+                        We have delivered
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className=" ml-5 ">
+                  <div className="flex w-full  ">
+                    {btn1 == true ? (
+                      <div className="">
+                        <div className="bg-bright-blue w-[5px] p-[4px] rounded-[50%] absolute top-[37rem] "></div>
+                        <div className="flex border min-w-full border-l-0 border-t-0 px-5 border-b-bright-blue border-r-bright-blue min-h-[3.8rem] relative left-0 right-0 bottom-0">
+                          {/* &nbsp; */}
+
+                          {/* <img
+                          src={linkLine}
+                          alt=""
+                          className="object-fill w-full h-[4rem]  ease-in-out"
+                        /> */}
+                        </div>
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                    {btn2 == true ? (
+                      <div className="">
+                        <div className="bg-bright-blue w-[5px] p-[4px] rounded-[50%] absolute top-[39.2rem]"></div>
+                        <div className="flex border min-w-full border-l-0 border-t-0 px-5 border-b-bright-blue border-r-bright-blue min-h-[6rem] relative">
+                          {/* <img
+                          src={linkLine}
+                          alt=""
+                          className="object-fill w-full h-[6.5rem]  ease-in-out"
+                        /> */}
+                        </div>
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                    {btn3 == true ? (
+                      <div className="">
+                        <div className="bg-bright-blue w-[5px] p-[4px] rounded-[50%] absolute top-[42.2rem]"></div>
+                        <div className="flex border min-w-full border-l-0 border-t-0 px-5 border-b-bright-blue border-r-bright-blue min-h-[9rem] relative">
+                          {/* <img
+                          src={linkLine}
+                          alt=""
+                          className="ml-2 object-fill  w-full h-[9rem]  "
+                        /> */}
+                        </div>
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                </div>
               </div>
             </div>
+            {/* old */}
+
+            {/* 
+            <div className=" text-white text-[0.8rem]">
+              <div className="flex items-end justify-between">
+                <div className="">
+                  <div className="list-none mb-3  font-semibold">
+                    <button onClick={() => setLinkText(arr[0])}>
+                      Who are we ?
+                    </button>
+                  </div>
+                  <div className="list-none mb-3">
+                    <button onClick={() => setLinkText(arr[1])}>
+                      What we do ?
+                    </button>
+                  </div>
+                  <div className="list-none ">
+                    <button onClick={() => setLinkText(arr[2])}>
+                      We have delivered
+                    </button>
+                  </div>
+                </div>
+                <div>
+                  <div className="">
+                    <img src={rightUpArrow} alt="rightuparrow" />
+                  </div>
+                </div>
+              </div>
+            </div> */}
           </div>
         </div>
         {/* Large screen */}
@@ -116,21 +249,23 @@ const Hero = () => {
                   </p>
                 </div>
               </div>
-              <div className=" mt-[4rem] text-white text-[1.2rem]">
-                <div className="list-none mb-7  font-semibold">
-                  <button onClick={() => setLinkText(arr[0])}>
-                    Who are we ?
-                  </button>
-                </div>
-                <div className="list-none mb-7">
-                  <button onClick={() => setLinkText(arr[1])}>
-                    Who are we ?
-                  </button>
-                </div>
-                <div className="list-none ">
-                  <button onClick={() => setLinkText(arr[2])}>
-                    Who are we ?
-                  </button>
+              <div className=" mt-[4rem] text-white text-[1.2rem] ">
+                <div>
+                  <div className="list-none mb-7  font-semibold">
+                    <button onClick={() => setLinkText(arr[0])}>
+                      Who are we ?
+                    </button>
+                  </div>
+                  <div className="list-none mb-7">
+                    <button onClick={() => setLinkText(arr[1])}>
+                      Who are we ?
+                    </button>
+                  </div>
+                  <div className="list-none ">
+                    <button onClick={() => setLinkText(arr[2])}>
+                      Who are we ?
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
--- src/components/common/HeroHlink.jsx
+++ src/components/common/HeroHlink.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-const HeroHlink = () => {
+// 👇️ named export
+export function Button1() {
   return (
     <div>
-      <p className="text-fade-gray text-[1.5rem] ">
+      <p className="text-fade-gray text-[1rem] lg:text-[1.5rem] xl:text-[2rem] p-3">
         We are a firm specialized in &nbsp;
         <span className="text-white font-bold">Machine Learning, AI</span>
         &nbsp; and &nbsp;
@@ -12,6 +13,33 @@ const HeroHlink = () => {
       </p>
     </div>
   );
-};
+}
 
-export default HeroHlink;
+// 👇️ named export
+export function Button2() {
+  return (
+    <div className="w-full">
+      <p className="text-fade-gray text-[1rem] lg:text-[1.5rem] xl:text-[2rem] p-3 ">
+        We deliver &nbsp;
+        <span className="text-white font-bold">values and ethics</span>
+        &nbsp; we understand &nbsp;
+        <span className="text-white font-bold"> your requirement.</span>
+        &nbsp;
+      </p>
+    </div>
+  );
+}
+
+export function Button3() {
+  return (
+    <div className="w-full">
+      <p className="text-fade-gray text-[1rem] lg:text-[1.5rem] xl:text-[2rem] p-3 ">
+        We deliver &nbsp;
+        <span className="text-white font-bold">values and ethics</span>
+        &nbsp; we understand &nbsp;
+        <span className="text-white font-bold"> your requirement.</span>
+        &nbsp;
+      </p>
+    </div>
+  );
+}
